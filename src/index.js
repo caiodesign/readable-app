72c06bb2a071c46b2be451eb4f3416ac7f2f3444
Caio Oliveira <caio.oliveira4@nextel.com.br>
2019-02-13 03:08:56 -0200

    added index.js file

--- src/index.js
+++ src/index.js
@@ -1,37 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import {BrowserRouter} from "react-router-dom";
-import {applyMiddleware, compose, createStore} from "redux";
-import reducer from "./reducers";
-import thunk from "redux-thunk";
-import {Provider} from "react-redux";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import './index.css'
+import reducer from './reducers'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-
-const logger = store => next => action => {
-    console.group(action.type)
-    console.info('dispatching', action)
-    let result = next(action)
-    console.log('next state', store.getState())
-    console.groupEnd(action.type)
-    return result
-}
-
+/* eslint no-underscore-dangle: 0 */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
-    reducer,
-    composeEnhancers(
-        applyMiddleware(logger, thunk)
-    )
-);
+  reducer,
+  composeEnhancers(applyMiddleware(thunk)),
+)
 
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter><App/></BrowserRouter>
-    </Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'),
+)
 
-registerServiceWorker();
+registerServiceWorker()
