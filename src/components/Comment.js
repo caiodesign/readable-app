fc9ed0ad0d62b1930d2a90738f2101c3b92f503e
Caio Oliveira <caio.oliveira4@nextel.com.br>
2019-02-13 01:42:42 -0200

    added comment files

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,163 +1,206 @@
-/**
- * Created by farid on 8/16/2017.
- */
-import React, {Component} from "react";
-import TimeAgo from "react-timeago";
-import {connect} from "react-redux";
-import {fetchDeleteComment, fetchDownVoteComment, fetchUpdateComment, fetchUpVoteComment} from "../actions/Comment";
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import TimeAgo from 'react-timeago'
+import {
+  fetchDeleteComment,
+  fetchDownVoteComment,
+  fetchUpdateComment,
+  fetchUpVoteComment,
+} from '../actions/Comment'
+
 
 class Comment extends Component {
+  state = {
+    isEditable: false,
+    comment: {},
+  };
 
-    state = {
-        isEditable: false,
-        comment: {}
-    };
+  componentDidMount() {
+    const { comment } = this.props
+    this.setState({ comment })
+  }
 
-    bodyOnChangeHandler = (event) => {
-        let newState = this.state;
-        newState.comment.body = event.target.value;
-        this.setState(newState)
-    };
+  componentWillReceiveProps(nextProps) {
+    const { comment } = nextProps
+    this.setState({ comment })
+  }
+
+  bodyOnChangeHandler = (event) => {
+    const newState = {}
+    const { value } = event.target
+    newState.comment.body = value
+    this.setState(newState)
+  };
 
     upVoteOnClickHandler = (event) => {
-        event.preventDefault();
-        this.props.upVoteComment(this.props.comment.id);
+      const { upVoteComment, comment } = this.props
+      event.preventDefault()
+      upVoteComment(comment.id)
     };
 
     downVoteOnClickHandler = (event) => {
-        event.preventDefault();
-        this.props.downVoteComment(this.props.comment.id);
+      const { downVoteComment, comment } = this.props
+      event.preventDefault()
+      downVoteComment(comment.id)
     };
 
-    editOnClickHandler = () => {
-        this.setState((prevState) => ({
-            isEditable: !prevState.isEditable
-        }))
-    };
+    editOnClickHandler = () => (
+      this.setState(prevState => ({ isEditable: !prevState.isEditable }))
+    )
 
     updateCommentOnClickHandler = () => {
-        this.props.updateComment(this.state.comment);
-        this.setState((prevState)=>({
-            isEditable:!prevState.isEditable
-        }));
+      const { updateComment } = this.props
+      const { comment } = this.state
+      updateComment(comment)
+      this.setState(prevState => ({
+        isEditable: !prevState.isEditable,
+      }))
     };
-
-    componentDidMount() {
-        this.setState({
-            comment: this.props.comment
-        });
-    }
 
     deleteCommentOnClickHandler = () => {
-        this.props.deleteComment(this.props.comment.id);
+      const { deleteComment, comment } = this.props
+      deleteComment(comment.id)
     };
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            comment: nextProps.comment
-        });
-    }
-
-
     render() {
-        let comment = this.state.comment;
-        return (
-            <div className="card">
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-md-1">
-                            <div className="btn-group-vertical btn-group-sm">
-                                <button type="button" className="btn btn-dark btn-sm float-left"><i
-                                    className="material-icons" onClick={this.upVoteOnClickHandler}>keyboard_arrow_up</i>
-                                </button>
-                                <button type="button" className="btn btn-dark btn-sm float-left"><i
-                                    className="material-icons"
-                                    onClick={this.downVoteOnClickHandler}>keyboard_arrow_down</i></button>
-                            </div>
-                        </div>
-                        <div className="col-md-11">
-                            <div className="row">
-                                <div className="col-md-12">
+      const { comment, isEditable } = this.state
+      const { isEditEnabled, isDeleteEnabled } = this.props
 
-                                    {this.state.isEditable === true ? (
-                                        <div>
-                                            <div className="form-group row">
-                                                <div className="col-sm-12">
-                                    <textarea type="text" className="form-control" id="body"
-                                              placeholder="Enter your comment text here" value={comment.body}
-                                              onChange={this.bodyOnChangeHandler}/>
-                                                </div>
-                                            </div>
-                                            <button className="btn btn-primary"
-                                                    onClick={this.updateCommentOnClickHandler}>Update
-                                            </button>
-                                            <br/>
-                                        </div>
-
-                                    ) : (
-                                        <div>
-                                            <p>
-                                                {comment.body}
-                                            </p>
-
-                                        </div>
-
-                                    )}
-                                </div>
-                                <div className="col-md-9">
-                                    <br/>
-                                    <p className="small">
-                                        Author: {comment.author} - Modified: <TimeAgo
-                                        date={new Date(comment.timestamp)}/> <br/>
-                                        Votes: {comment.voteScore}
-                                    </p>
-                                </div>
-                                <div className="col-md-3">
-
-                                    {
-                                        this.props.isDeleteEnabled && (
-                                            <button type="button" className="btn btn-danger btn-sm float-right"><i
-                                                className="material-icons" onClick={this.deleteCommentOnClickHandler}>delete</i>
-                                            </button>
-                                        )
-                                    }
-                                    {
-                                        this.props.isEditEnabled && (
-                                            <button type="button" className="btn btn-primary btn-sm float-right"
-                                                    onClick={this.editOnClickHandler}><i className="material-icons">mode_edit</i>
-                                            </button>
-                                        )
-                                    }
-                                </div>
-
-
-                            </div>
-
-                        </div>
-                    </div>
-
+      return (
+        <div className="card">
+          <div className="card-body">
+            <div className="row">
+              <div className="col-md-1">
+                <div className="btn-group-vertical btn-group-sm">
+                  <button
+                    type="button"
+                    className="btn btn-dark btn-sm float-left"
+                  >
+                    <i
+                      tabIndex={0}
+                      role="button"
+                      className="material-icons"
+                      onClick={this.upVoteOnClickHandler}
+                    >
+                      keyboard_arrow_up
+                    </i>
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-dark btn-sm float-left"
+                  >
+                    <i
+                      tabIndex={0}
+                      role="button"
+                      className="material-icons"
+                      onClick={this.downVoteOnClickHandler}
+                    >
+                      keyboard_arrow_down
+                    </i>
+                  </button>
                 </div>
+              </div>
+              <div className="col-md-11">
+                <div className="row">
+                  <div className="col-md-12">
+                    {isEditable === true ? (
+                      <div>
+                        <div className="form-group row">
+                          <div className="col-sm-12">
+                            <textarea
+                              type="text"
+                              className="form-control"
+                              id="body"
+                              placeholder="Enter your comment text here"
+                              value={comment.body}
+                              onChange={this.bodyOnChangeHandler}
+                            />
+                          </div>
+                        </div>
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          onClick={this.updateCommentOnClickHandler}
+                        >
+                          Update
+                        </button>
+                        <br />
+                      </div>
+                    ) : (
+                      <div>
+                        <p>
+                          { comment.body }
+                        </p>
+                      </div>
+                    )}
+                  </div>
+                  <div className="col-md-9">
+                    <br />
+                    <p className="small">
+                      Author:
+                      { comment.author }
+                      - Modified:
+                      <TimeAgo date={new Date(comment.timestamp)} />
+                      <br />
+                      Votes:
+                      { comment.voteScore }
+                    </p>
+                  </div>
+                  <div className="col-md-3">
+
+                    {isDeleteEnabled && (
+                      <button
+                        type="button"
+                        className="btn btn-danger btn-sm float-right"
+                      >
+                        <i
+                          tabIndex={0}
+                          role="button"
+                          className="material-icons"
+                          onClick={this.deleteCommentOnClickHandler}
+                        >
+                          delete
+                        </i>
+                      </button>
+                    )}
+                    {isEditEnabled && (
+                      <button
+                        type="button"
+                        className="btn btn-primary btn-sm float-right"
+                        onClick={this.editOnClickHandler}
+                      >
+                        <i className="material-icons">mode_edit</i>
+                      </button>
+                    )}
+                  </div>
+                </div>
+              </div>
             </div>
-
-
-        )
+          </div>
+        </div>
+      )
     }
 }
 
 Comment.propTypes = {
-    comment: PropTypes.object.isRequired
-};
+  comment: PropTypes.object.isRequired,
+  upVoteComment: PropTypes.func,
+  downVoteComment: PropTypes.func,
+  updateComment: PropTypes.func,
+  deleteComment: PropTypes.func,
+  isEditEnabled: PropTypes.bool,
+  isDeleteEnabled: PropTypes.bool,
+}
 
-const mapDispatchToProps = (dispatch) => ({
-    upVoteComment: (commentId) => dispatch(fetchUpVoteComment(commentId)),
-    downVoteComment: (commentId) => dispatch(fetchDownVoteComment(commentId)),
-    deleteComment: (commentId) => dispatch(fetchDeleteComment(commentId)),
-    updateComment: (comment) => dispatch(fetchUpdateComment(comment))
-});
-
+const mapDispatchToProps = dispatch => ({
+  upVoteComment: commentId => dispatch(fetchUpVoteComment(commentId)),
+  downVoteComment: commentId => dispatch(fetchDownVoteComment(commentId)),
+  deleteComment: commentId => dispatch(fetchDeleteComment(commentId)),
+  updateComment: comment => dispatch(fetchUpdateComment(comment)),
+})
 
 export default connect(
-    null,
-    mapDispatchToProps
+  null,
+  mapDispatchToProps,
 )(Comment)
--- src/components/CreateComment.js
+++ src/components/CreateComment.js
@@ -1,86 +1,100 @@
-/**
- * Created by farid on 8/16/2017.
- */
-import React, {Component} from "react";
-import PropTypes from "prop-types";
-import {connect} from "react-redux";
-import {fetchCreateComment} from "../actions/Comment";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { fetchCreateComment } from '../actions/Comment'
 
 
 class CreateComment extends Component {
+  state = {
+    body: '',
+    author: '',
+  }
 
+  createComment = () => {
+    const { createComment, parentId } = this.props
+    const { body, author } = this.state
 
-    state = {
-        body: '',
-        author: ''
+    createComment({
+      body,
+      author,
+      parentId,
+    })
 
-    };
+    this.setState({
+      body: '',
+      author: '',
+    })
+  }
 
-    createComment = () => {
-        this.props.createComment({
-            body: this.state.body,
-            author: this.state.author,
-            parentId: this.props.parentId
-        })
-        this.setState({
-            body: '',
-            author: ''
-        })
-    };
+  bodyOnChangeHandler = (event) => {
+    const { value } = event.target
+    this.setState({ body: value })
+  }
 
+  authorOnChangeHandler = (event) => {
+    const { value } = event.target
+    this.setState({ author: value })
+  }
 
-    bodyOnChangeHandler = (event) => {
-        this.setState({
-            body: event.target.value
-        })
-    };
+  render() {
+    const { body, author } = this.state
 
-    authorOnChangeHandler = (event) => {
-        this.setState({
-            author: event.target.value
-        })
-    };
-
-    render() {
-        return (
-            <div className="card">
-                <div className="card-body">
-                    <div className="form-group row">
-                        <div className="col-sm-10">
-                            <textarea type="text" className="form-control" id="body"
-                                      placeholder="Enter your comment text here" onChange={this.bodyOnChangeHandler}
-                                      value={this.state.body}/>
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <div className="col-sm-10">
-                            <input type="text" className="form-control" id="author"
-                                   placeholder="Enter author here" onChange={this.authorOnChangeHandler}
-                                   value={this.state.author}/>
-                        </div>
-                    </div>
-                    <div className="form-group row float-right">
-                        <div className="col-sm-2 ">
-                            <button type="submit" className="btn btn-primary" onClick={this.createComment}>Create
-                            </button>
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div className="card">
+        <div className="card-body">
+          <div className="form-group row">
+            <div className="col-sm-10">
+              <textarea
+                type="text"
+                className="form-control"
+                id="body"
+                placeholder="Enter your comment text here"
+                onChange={this.bodyOnChangeHandler}
+                value={body}
+              />
             </div>
-        )
-    }
+          </div>
+          <div className="form-group row">
+            <div className="col-sm-10">
+              <input
+                type="text"
+                className="form-control"
+                id="author"
+                placeholder="Enter author here"
+                onChange={this.authorOnChangeHandler}
+                value={author}
+              />
+            </div>
+          </div>
+          <div className="form-group row float-right">
+            <div className="col-sm-2 ">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={this.createComment}
+              >
+                Create
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 
-const mapDispatchToProps = (dispatch) => ({
-    createComment: (comment) => dispatch(fetchCreateComment(comment))
-});
+const mapDispatchToProps = dispatch => ({
+  createComment: comment => dispatch(fetchCreateComment(comment)),
+})
 
 
 CreateComment.propTypes = {
-    parentId: PropTypes.string.isRequired
-};
+  parentId: PropTypes.string.isRequired,
+  createComment: PropTypes.func,
+}
+
 export default connect(
-    null,
-    mapDispatchToProps
+  null,
+  mapDispatchToProps,
 )(CreateComment)
