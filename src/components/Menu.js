7c4616aeba2367328e87074b30419459cb8602dd
Caio Oliveira <caio.oliveira4@nextel.com.br>
2019-02-15 22:29:43 -0200

    removed xd from title

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,7 +8,7 @@ class Menu extends Component {
       <div>
         <nav className="navbar navbar-light bg-light justify-content-center">
           <Link to="/" className="navbar-brand text-center">
-            MY AWESOME WORDPRESS.js xD
+            MY AWESOME WORDPRESS.js
           </Link>
         </nav>
       </div>
